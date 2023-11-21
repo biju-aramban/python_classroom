dd630f228b02644f0dae5373f3701bf07dd56f98
biju-aramban <40836824+biju-aramban@users.noreply.github.com>
2023-11-21 10:33:24 +0530

    Create 01_greet.py
    Greet someone.
    Hello someone program.

---
+++ classrooms/week1/01_greet.py
@@ -0,0 +1,13 @@
+import os
+
+
+def greet():
+    
+    my_name = input("Please enter your name: ")
+    print(f"Hello {my_name}, Have a great day.")
+    
+
+if __name__ == "__main__":
+    greet()
+
+
