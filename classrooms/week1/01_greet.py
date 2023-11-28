84676b55c37c0ce061fa50883e0a8e03f5f4fa38
biju-aramban <40836824+biju-aramban@users.noreply.github.com>
2023-11-28 10:09:49 +0530

    Update 01_greet.py

--- classrooms/week1/01_greet.py
+++ classrooms/week1/01_greet.py
@@ -1,13 +1,21 @@
+# This is my first Python Program
+# Author: Biju Aramban
+
+"""
+This is how you write a multiline comment in Python.
+Multi line comments are also called as docstrings.
+docstrings are used to represent any kind of documentation in Python as well.
+This is my first Python Program
+I will be using this to learn Python
+"""
+
 import os
 
 
-def greet():
-    
-    my_name = input("Please enter your name: ")
-    print(f"Hello {my_name}, Have a great day.")
-    
+def greet_the_world():
+    print("Hello World!")
+
 
 if __name__ == "__main__":
-    greet()
-
-
+    greet_the_world()
+    print("I am done with my first python program.")
